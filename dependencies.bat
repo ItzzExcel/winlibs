94e1e2fcde528de0f03cc6712ef7580f7580edb7
ItzzExcel <98148217+ItzzExcel@users.noreply.github.com>
2023-06-05 22:50:41 -0600

    Add files via upload

---
+++ dependencies.bat
@@ -0,0 +1,45 @@
+@echo off
+color a
+
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "%~s0", "%params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+set "url1=https://github.com/ItzzExcel/winlibs/raw/main/Libs/vcruntime140_1d.dll"
+set "url2=https://github.com/ItzzExcel/winlibs/raw/main/Libs/msvcp140d.dll"
+set "url3=https://github.com/ItzzExcel/winlibs/raw/main/Libs/vcruntime140d.dll"
+set "url4=https://github.com/ItzzExcel/winlibs/raw/main/Libs/ucrtbased.dll"
+
+set "dest32=C:\Windows\System32"
+set "dest64=C:\Windows\SysWOW64"
+
+echo "[https://github.com/ItzzExcel/winlibs / Dependency Installer (SAFE TO RUN)]"
+
+echo Downloading dependencies . . .
+
+echo Downloading for System32 . . .
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%url1%', '%dest32%\vcruntime140_1d.dll')"
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%url2%', '%dest32%\msvcp140d.dll')"
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%url3%', '%dest32%\vcruntime140d.dll')"
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%url4%', '%dest32%\ucrtbased.dll')"
+
+echo Downloading for SysWOW64 . . .
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%url1%', '%dest64%\vcruntime140_1d.dll')"
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%url2%', '%dest64%\msvcp140d.dll')"
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%url3%', '%dest64%\vcruntime140d.dll')"
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%url4%', '%dest64%\ucrtbased.dll')"
